ff6af6e505078569bc7e25c65aea7e8d2b176266
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-10-10 20:06:14 +0200

    User model has been implemented

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const { Schema, mongoose } = require("mongoose");
+
+export const UserSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", UserSchema);
